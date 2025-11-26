3d2cb84f17ac1013ac6daf6fc85cd50362bcfb03
AmandaLismawati <149929067+AmandaLismawati@users.noreply.github.com>
2025-11-26 11:14:09 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Api\FakultasController;
+use App\Http\Controllers\Api\ProdiController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::get('/fakultas',[FakultasController::class, 'index']);
+Route::get('/prodi',[ProdiController::class, 'index']);
